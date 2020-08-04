91ccd40668f5a4c05d9f7ff957a7c48082c00372
Jonathan Singer <jonathan.s.singer@gmail.com>
2020-08-04 12:54:08 -0500

    Make timeline show more recent posts first

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -16,6 +16,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var tableView: UITableView!
     
     var posts = [PFObject]()
+    var feed = [PFObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,18 +36,21 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         query.findObjectsInBackground { (posts, error) in
             if posts != nil {
                 self.posts = posts!
+                for post in self.posts {
+                    self.feed.insert(post, at: 0)
+                }
                 self.tableView.reloadData()
             }
         }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
+        return feed.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
-        let post = posts[indexPath.row]
+        let post = feed[indexPath.row]
         
         let user = post["author"] as! PFUser
         cell.userNameLabel.text = user.username
