b7b6cbdfc6cbd4125691c9bdd6488e8956817b36
Jonathan Singer <jonathan.s.singer@gmail.com>
2020-08-04 22:01:12 -0500

    Add profile page functionality

--- Parstagram/ProfilesViewController.swift
+++ Parstagram/ProfilesViewController.swift
@@ -7,24 +7,59 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class ProfilesViewController: UIViewController {
+class ProfilesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+        let query = PFQuery(className:"Posts")
+        query.whereKey("author", equalTo: PFUser.current()!)
+        query.includeKey("author")
+        query.order(byDescending: "createdAt")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser
+        cell.userNameLabel.text = user.username
+        cell.captionLabel.text = (post["caption"] as! String)
+        self.title = user.username
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af.setImage(withURL: url)
+        
+        return cell
+    }
+    
 }
